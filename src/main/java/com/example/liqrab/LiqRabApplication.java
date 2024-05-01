1467a17f5bb4f514551e8ad3faa4787728afc223
visels <elviskipchumba@yahoo.com>
2024-05-01 13:00:17 +0300

    initial setup

---
+++ src/main/java/com/example/liqrab/LiqRabApplication.java
@@ -0,0 +1,13 @@
+package com.example.liqrab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiqRabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiqRabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/liqrab/LiqRabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liqrab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiqRabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
